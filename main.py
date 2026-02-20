3a4a7d55be286c7cec4ccad7fdc86f8fb5a656fe
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-02-20 22:29:09 +0530

    Create student profile program using string, int, float and boolean.

---
+++ main.py
@@ -0,0 +1,10 @@
+student_name="chai"
+student_age=20
+student_height=5.3
+student_is_passed=True
+print("~~~~Student Details~~~~")
+print(f"Student name:{student_name}")
+print(f"Student age:{student_age}")
+print(f"Student height:{student_height}")
+print(f"Student is passed:{student_is_passed}")
+print("~~~~~~~~~~~~~~~")
