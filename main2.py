441a9c73ab36c3184fb59d3e7ad5927770f82c02
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-03-02 22:18:02 +0530

    Practice: Count even and odd numbers in a list.

--- main2.py
+++ main2.py
@@ -1,4 +1,14 @@
-tab=int(input("Enter a number:"))
-for i in range(1,11):
-    count=tab*i
-    print(tab,"X",i,"=",count)
+numbers=[]
+even_count=0
+odd_count=0
+for i in range(10):
+    num=int(input("Enter number:"))
+    numbers.append(num)
+    
+    if num%2==0:
+        even_count+=1
+    else:
+        odd_count+=1
+print(even_count)
+print(odd_count)
+print(numbers)
