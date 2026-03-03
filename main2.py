548ee30dcba310f29c1c57c0bc325ab6f8ca1007
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-03-03 21:59:56 +0530

    Practice: Reverse list using reverse() method.

--- main2.py
+++ main2.py
@@ -1,14 +1,3 @@
-numbers=[]
-even_count=0
-odd_count=0
-for i in range(10):
-    num=int(input("Enter number:"))
-    numbers.append(num)
-    
-    if num%2==0:
-        even_count+=1
-    else:
-        odd_count+=1
-print(even_count)
-print(odd_count)
-print(numbers)
+num=[1,2,3,4,5]
+num.reverse()
+print(num)
