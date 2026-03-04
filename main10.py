3391ed6cc7540a8f027e4fb1a745c903feee7425
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-03-04 22:14:25 +0530

    Pratice:Added list extend() example in Python.

--- main10.py
+++ main10.py
@@ -1,18 +1,3 @@
-password= " "
-password=input("Enter Password:")
-has_upper=False
-has_lower=False
-has_digit=False
-for i in password:
-    if i.isupper():
-        has_upper=True
-    if i.islower():
-        has_lower=True
-    if i.isdigit():
-        has_digit=True
-if len(password)>=8 and has_upper and has_lower and has_digit:
-    print("Strong password")
-else:
-    print("Weak password")     
-    
-    
+num=[1,2,3]
+num.extend([4,5,6])
+print(num)
