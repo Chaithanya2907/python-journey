4b2cc4cd537fa5b2381fdbdda2d199575875ff20
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-03-23 21:58:05 +0530

    Pratice : program to check divisibility by 3 and 5.

--- main1.py
+++ main1.py
@@ -1,10 +1,9 @@
-number=[]
-for i in range(5):
-    num=int(input("Enter number:"))
-    number.append(num)
-search=int(input("Enter count num:"))
-count=number.count(search)
-print("num",count, "appers",search,"times")
-
-    
-    
+num=int(input("Enter a number:"))
+if num%3==0 and num%5==0:
+    print("Divisible by both 3 and 5")
+elif num%3==0:
+    print("divi by 3")
+elif num%5==0:
+    print("divi by 5")
+else:
+    print("Either")
