fa6927d72c5c5b6c135de9ba50e97701cb5013ed
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-03-23 21:59:04 +0530

    Pratice : Check number type (positive/negative/zero) and even/odd using Python.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-student_name="chai"
-student_age=20
-student_height=5.3
-student_is_passed=True
-print("~~~~Student Details~~~~")
-print(f"Student name:{student_name}")
-print(f"Student age:{student_age}")
-print(f"Student height:{student_height}")
-print(f"Student is passed:{student_is_passed}")
-print("~~~~~~~~~~~~~~~")
+number=int(input("Enter a number:"))
+if number>0:
+    print("positive")
+elif number<0:
+    print("Negative")
+else:
+    print("Zero")
+if number%2==0:
+    print("even")
+else:
+    print("Odd")
+    
